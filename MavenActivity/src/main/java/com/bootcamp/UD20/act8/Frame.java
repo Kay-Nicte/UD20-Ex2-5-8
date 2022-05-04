cf158f21d19c35e3370d9a15ea93cd2c91fff9a0
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-05-04 18:54:49 +0200

    "Añadir try catch para validar valor numérico"

--- MavenActivity/src/main/java/com/bootcamp/UD20/act8/Frame.java
+++ MavenActivity/src/main/java/com/bootcamp/UD20/act8/Frame.java
@@ -46,66 +46,80 @@ public class Frame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Cantidad a convertir");
 		lblNewLabel.setBounds(32, 46, 122, 13);
 		contentPane.add(lblNewLabel);
-		
+
 		textField_Cantidad = new JTextField();
 		textField_Cantidad.setBounds(164, 42, 60, 19);
 		contentPane.add(textField_Cantidad);
 		textField_Cantidad.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Resultado");
 		lblNewLabel_1.setBounds(244, 45, 78, 13);
 		contentPane.add(lblNewLabel_1);
-		
+
 		textField_Resultado = new JTextField();
 		textField_Resultado.setBounds(330, 42, 96, 19);
 		textField_Resultado.setBackground(Color.WHITE);
 		contentPane.add(textField_Resultado);
 		textField_Resultado.setColumns(10);
-		
-		//Botón para convertir
+
+		// Botón para convertir
 		JButton btnNewButton = new JButton("Euros a ptas");
 		btnNewButton.setBounds(65, 91, 135, 21);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				double pesetas = Double.valueOf(textField_Cantidad.getText())*166.386;
-				textField_Resultado.setText(String.valueOf(pesetas)); 
-								
+
+				String str = String.valueOf(textField_Cantidad.getText());
+
+				/*
+				 * if(isNumeric) {
+				 * 
+				 * } else{ textField_Resultado.setText("No es un número");
+				 * //System.out.println(!isNumeric); }
+				 */
+
+				try {
+					boolean isNumeric = str.matches("[+-]?\\d*(\\.\\d+)?");
+					double pesetas = Double.valueOf(textField_Cantidad.getText()) * 166.386;
+					textField_Resultado.setText(String.valueOf(pesetas));
+				} catch (Exception e1) {
+					textField_Resultado.setText("Valor incorrecto");
+				} 
 			}
 		});
+
 		contentPane.add(btnNewButton);
-		
-		//Botón para cambiar de moneda
+
+		// Botón para cambiar de moneda
 		JButton btnNewButton_1 = new JButton("Cambiar");
 		btnNewButton_1.setBounds(210, 91, 101, 21);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				JOptionPane.showMessageDialog(null, "Modo Pesetas -> Euros");
-				double euros = Double.valueOf(textField_Cantidad.getText().toString())/166.386;
-				textField_Resultado.setText(String.valueOf(euros)); 
-				
+				double euros = Double.valueOf(textField_Cantidad.getText().toString()) / 166.386;
+				textField_Resultado.setText(String.valueOf(euros));
+
 			}
 		});
+
 		contentPane.add(btnNewButton_1);
 		contentPane.add(btnNewButton);
-		
-		//Botón para borrar el contenido
+
+		// Botón para borrar el contenido
 		JButton btnNewButton_2 = new JButton("Borrar");
 		btnNewButton_2.setBounds(330, 91, 101, 21);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				textField_Cantidad.setText("");
-				textField_Resultado.setText(""); 
-				
+				textField_Resultado.setText("");
+
 			}
 		});
 		contentPane.add(btnNewButton_2);
 	}
-
 }
